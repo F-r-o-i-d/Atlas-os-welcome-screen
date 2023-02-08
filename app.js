ed8cf64a600feebc46a6dcdfd9cb4a0858a931d5
F-r-o-i-d <82611242+F-r-o-i-d@users.noreply.github.com>
2023-02-08 23:35:10 +0100

    first commit (probably the last too)

---
+++ app.js
@@ -0,0 +1,58 @@
+const electron = require('electron');
+const url = require('url');
+const path = require('path');
+const os = require('os');
+// Components
+const menuBar = require('./src/MenuBar');
+
+const { app, BrowserWindow, Menu, ipcMain } = electron;
+
+let mainWindow;
+
+//c
+
+app.on('ready', () => {
+
+  mainWindow = new BrowserWindow({
+    // width: width,
+    // height: height,
+    //hide menu bar
+    autoHideMenuBar: true,
+    fullscreen: true,
+    
+    webPreferences: { nodeIntegration: true,
+      alwaysOnTop: true,
+      contextIsolation: false,
+      nativeWindowOpen: true,
+      enableRemoteModule: true,
+      sandbox:false,
+      nodeIntegrationInSubFrames:true, //for subContent nodeIntegration Enable
+      webviewTag:true //for webView,
+    },
+    preload: path.join(__dirname, 'src/preload.js')
+    
+  });
+
+  if (os.platform() != 'win32') {
+    mainWindow.webContents.openDevTools();
+  }
+  mainWindow.setAlwaysOnTop(true, "floating");
+  mainWindow.setVisibleOnAllWorkspaces(true);
+  mainWindow.setFullScreenable(false);
+
+  mainWindow.loadURL(url.format({
+    // All this is doing is passing in to file://your_directory/src/index.html
+    pathname: path.join(__dirname, 'src/index.html'),
+    //allow dev tools
+    protocol: 'file:',
+    slashes: true
+  }));
+
+  // Shut down app when window closes
+  mainWindow.on('closed', () => app.quit()); 
+
+  const mainMenu = Menu.buildFromTemplate(menuBar);
+  Menu.setApplicationMenu(mainMenu);
+  mainWindow.webContents.openDevTools();
+
+});
